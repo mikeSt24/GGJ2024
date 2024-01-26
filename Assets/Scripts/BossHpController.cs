043ac9bfdae5c068d324a02565981d5516324c03
DESKTOP-C16NADV\The Hulk Machine <alcanforadoalto@gmail.com>
2024-01-27 00:07:32 +0100

    Hud Player & Boss (Basic)

---
+++ Assets/Scripts/BossHpController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHpController: MonoBehaviour
+{
+    public Image healthBar;
+    public float BaseHealth = 100.0f;
+    public float DamageoOfHit = 20.00f;
+    public float HealthAmount = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Hitreceived(DamageoOfHit);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ResetHealth();
+        }
+    }
+
+    public void Hitreceived(float dmg)
+    {
+        HealthAmount -= dmg;
+        healthBar.fillAmount = HealthAmount / 100.0f;
+    }
+
+    public void ResetHealth()
+    {
+        HealthAmount = BaseHealth;
+        healthBar.fillAmount = HealthAmount;
+    }
+}
