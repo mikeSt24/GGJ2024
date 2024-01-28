31320fd0ce45dca8118fa1f9db20540375542b42
jonu2 <jon.urrutiab@gmail.com>
2024-01-28 11:31:38 +0100

    small bug

--- Assets/Scripts/PauseButoonBehaviour.cs
+++ Assets/Scripts/PauseButoonBehaviour.cs
@@ -8,6 +8,7 @@ public class PauseButoonBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     public Animator player_anim;
+    private bool active = false;
 
     void Start()
     {
@@ -27,66 +28,36 @@ public class PauseButoonBehaviour : MonoBehaviour
 
     public void ActivatePauseMenu()
     {
-        if(player_anim.GetBool("Countdown"))
+        if(player_anim.GetBool("Countdown") || active)
         {
             return;
         }
-
+        active = true;
         Time.timeScale = 0.0f;
         for (int i = 0; i < transform.childCount; i++)
         {
-            //if(transform.GetChild(i).name == "PauseImages")
-            //{
-            //    for (int j = 0; j < transform.GetChild(i).transform.childCount; j++)
-            //    {
-            //        transform.GetChild(i).transform.GetChild(j).gameObject.SetActive(true);
-            //    }
-            //}
             transform.GetChild(i).gameObject.SetActive(true);
         }
 
-        //gameObject.GetComponent<Button>().enabled = false;
-        //gameObject.GetComponent<Image>().enabled = false;
     }
 
     public void ResumeGame()
     {
 
-        Debug.Log("Goo");
+        active = false;
         Time.timeScale = 1.0f;
         for (int i = 0; i < transform.childCount; i++)
         {
-            //if (transform.GetChild(i).name == "PauseImages")
-            //{
-            //    for (int j = 0; j < transform.GetChild(i).transform.childCount; j++)
-            //    {
-            //        transform.GetChild(i).transform.GetChild(j).gameObject.SetActive(false);
-            //    }
-            //}
             transform.GetChild(i).gameObject.SetActive(false);
         }
 
-        //transform.GetComponent<Button>().enabled = true;
-        //transform.GetComponent<Image>().enabled = true;
     }
     public void OptionsButton()
     {
-        Debug.Log("1");
 
         GameObject OptionsBut = gameObject;
         for (int i = 0; i < transform.childCount; i++)
         {
-            //if (transform.GetChild(i).name == "PauseImages")
-            //{
-            //    for (int j = 0; j < transform.GetChild(i).transform.childCount; j++)
-            //    {
-            //        //if (OptionsBut.transform.GetChild(i).gameObject.name != "Text (TMP)")
-            //        //{
-            //        //    OptionsBut.transform.GetChild(i).gameObject.GetComponent<Image>().enabled = false;
-            //        //}
-            //        OptionsBut.transform.GetChild(i).gameObject.SetActive(false);
-            //    }
-            //}
             if (transform.GetChild(i).name == "OPTIONS")
             {
                 OptionsBut = transform.GetChild(i).gameObject;
@@ -102,19 +73,11 @@ public class PauseButoonBehaviour : MonoBehaviour
             OptionsBut.transform.GetChild(i).gameObject.SetActive(true);
         }
 
-        Debug.Log("2");
     }
     public void ExitOptionsButton()
     {
         for (int i = 0; i < transform.childCount; i++)
         {
-            //if (transform.GetChild(i).name == "PauseImages")
-            //{
-            //    for (int j = 0; j < transform.GetChild(i).transform.childCount; j++)
-            //    {
-            //        transform.GetChild(i).transform.GetChild(j).gameObject.SetActive(true);
-            //    }
-            //}
             if (transform.GetChild(i).name == "OPTIONS")
             {
                 for (int j = 0; j < transform.GetChild(i).transform.childCount; j++)
@@ -133,6 +96,7 @@ public class PauseButoonBehaviour : MonoBehaviour
     }
     public void ExitGame()
     {
+        active = false;
         Time.timeScale = 1.0f;
         SceneManager.LoadScene("MainMenu");
     }
